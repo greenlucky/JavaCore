1d0ed789236d9524356241eaae2b11f43a9801c2
Lam Nguyễn <nguyenlamit86@gmail.com>
2018-02-01 21:53:35 +0700

    Add lamda functional

---
+++ src/main/java/com/lamdevops/stream/filter/FilterTest.java
@@ -0,0 +1,52 @@
+package com.lamdevops.stream.filter;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * Created by lam.nm on 7/25/2017.
+ */
+public class FilterTest
+{
+    @Test
+    public void filter() {
+       String word = "This is example of stream spit";
+
+        Stream<String> words = Stream.of(word.split(" "));
+        print(words);
+    }
+
+    @Test
+    public void generationString() throws Exception {
+        Stream<String> echos = Stream.generate(()-> "Echo");
+        print(echos);
+    }
+
+    @Test
+    public void generationNumbers() throws Exception {
+
+        Stream<Double> randoms = Stream.generate(Math::random);
+        print(randoms);
+    }
+
+    @Test
+    public void fileLine() throws Exception {
+        File file = new File("C:\\Users\\lam.nm\\Documents\\JavaCore\\src\\com\\lamdevops\\stream\\testfile.txt");
+        Stream<String> lines = Files.lines(file.toPath()).filter(str->str.contains("line") && str.length() > 15).map(String::toLowerCase);
+        print(lines);
+    }
+
+    @Test
+    public void notNull() throws Exception {
+        Stream<String> language = Stream.of("java", "python", "node", null, "ruby", null, "php");
+        language.filter(Objects::nonNull).forEach(System.out::println);
+    }
+
+    public static void print(Stream<?> strs) {
+        strs.forEach(System.out::println);
+    }
+}
